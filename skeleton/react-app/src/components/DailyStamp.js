28757435fd2384b3bb1fd18f3adc3fcf144e8ee8
Brian Wang <brbwang@yahoo.com>
2020-11-23 18:22:54 -0500

    new component with basic css

---
+++ skeleton/react-app/src/components/DailyStamp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DailyStamp = () => {
+  return (
+    <div class='stamp-container'>
+      <div class='stamp-box'>
+
+      </div>
+    </div>
+  );
+}
+
+export default newComponent;
